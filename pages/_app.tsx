f675e974184fc74d1b52d0f26c8b2a262b433ebd
Archie <archie.yang@dynamocover.com>
2023-07-13 14:47:43 +0100

    added web3

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { Web3Provider } from "@/components/Web3Context";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Web3Provider>
+      <Component {...pageProps} />
+    </Web3Provider>
+  );
 }
